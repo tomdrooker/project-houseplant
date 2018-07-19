8f6ca7f8dccf86904967db16e405d64536970d82
Tom Rooker <tomdrooker@gmail.com>
2018-07-19 21:38:18 +0100

    Quiz functioning

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -1,6 +1,10 @@
 const quizSection = document.querySelector("#quiz");
 const submitButton = document.querySelector("#submit");
 const resultsSection = document.querySelector("#results");
+const previousButton = document.querySelector("#previous");
+const nextButton = document.querySelector("#next");
+const slides = document.getElementsByClassName("slide");
+let currentSlide = 0;
 
 const quizData = [
   {
@@ -47,8 +51,10 @@ function buildQuiz() {
       }
       output.push(
         `
-        <div class="question"> ${currentQuestion.question} </div>
-        <div class="answers">  ${answers.join('')} </div>
+        <div class="slide">
+          <div class="question"> ${currentQuestion.question} </div>
+          <div class="answers">  ${answers.join('')} </div>
+        </div>
         `
       );
     }
@@ -80,6 +86,39 @@ function showResults() {
   resultsSection.innerHTML = output.join('');
 };
 
+function showSlide(n) {
+  slides[currentSlide].classList.remove("active-slide");
+  slides[n].classList.add("active-slide");
+  currentSlide = n;
+
+  if (currentSlide === 0) {
+    previousButton.style.display = "none";
+  } else {
+    previousButton.style.display = "inline-block";
+  }
+
+  if (currentSlide === slides.length - 1) {
+    nextButton.style.display = "none";
+    submitButton.style.display = "inline-block";
+  } else {
+    nextButton.style.display = "inline-block";
+    submitButton.style.display = "none";
+  }
+};
+
+function showNextSlide() {
+  showSlide(currentSlide + 1);
+};
+
+
+function showPreviousSlide() {
+  showSlide(currentSlide - 1);
+};
+
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
 buildQuiz();
+showSlide(2);
 
 submitButton.addEventListener("click", showResults);
